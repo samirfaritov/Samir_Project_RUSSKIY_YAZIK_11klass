3e9e40208f30c16b6989f86959fd4ded83c36116
samirfaritov <samirfaritov6@gmail.com>
2023-12-08 16:30:10 +0500

    samir project russkiy yazik 11 klass

---
+++ main.js
@@ -0,0 +1,343 @@
+let item = document.querySelector('.item')
+let item2 = document.querySelector('.item2')
+let item3 = document.querySelector('.item3')
+let item4 = document.querySelector('.item4')
+let item5 = document.querySelector('.item5')
+let item6 = document.querySelector('.item6')
+let item7 = document.querySelector('.item7')
+let item9 = document.querySelector('.item9')
+let item10 = document.querySelector('.item10')
+let item11 = document.querySelector('.item11')
+let item12 = document.querySelector('.item12')
+let item13 = document.querySelector('.item13')
+let item14 = document.querySelector('.item14')
+let item15 = document.querySelector('.item15')
+let item16 = document.querySelector('.item16')
+let item17 = document.querySelector('.item17')
+let item18 = document.querySelector('.item18')
+let item19 = document.querySelector('.item19')
+let item20 = document.querySelector('.item20')
+let item21 = document.querySelector('.item21')
+let item22 = document.querySelector('.item22')
+let item23 = document.querySelector('.item23')
+let item24 = document.querySelector('.item24')
+let item25 = document.querySelector('.item25')
+let item26 = document.querySelector('.item26')
+let item27 = document.querySelector('.item27')
+let item28 = document.querySelector('.item28')
+let item29 = document.querySelector('.item29')
+let item30 = document.querySelector('.item30')
+let item31 = document.querySelector('.item31')
+
+
+let content = document.querySelector('.content')
+let content2 = document.querySelector('.content2')
+let content3 = document.querySelector('.content3')
+let content4 = document.querySelector('.content4')
+let content5 = document.querySelector('.content5')
+let content6 = document.querySelector('.content6')
+let content7 = document.querySelector('.content7')
+let content8 = document.querySelector('.content8')
+let content9 = document.querySelector('.content9')
+let content10 = document.querySelector('.content10')
+let content11 = document.querySelector('.content11')
+let content12 = document.querySelector('.content12')
+let content13 = document.querySelector('.content13')
+let content14 = document.querySelector('.content14')
+let content15 = document.querySelector('.content15')
+let content16 = document.querySelector('.content16')
+let content17 = document.querySelector('.content17')
+let content18 = document.querySelector('.content18')
+let content19 = document.querySelector('.content19')
+let content20 = document.querySelector('.content20')
+let content21 = document.querySelector('.content21')
+let content22 = document.querySelector('.content22')
+let content23 = document.querySelector('.content23')
+let content24 = document.querySelector('.content24')
+let content25 = document.querySelector('.content25')
+let content26 = document.querySelector('.content26')
+let content27 = document.querySelector('.content27')
+let content28 = document.querySelector('.content28')
+let content29 = document.querySelector('.content29')
+let content30 = document.querySelector('.content30')
+
+
+
+
+item.addEventListener('click', () => {
+    content.style.display = 'block'
+})
+item2.addEventListener('click', () => {
+    console.log('click');
+    
+    content2.style.display = 'block'
+
+})
+item3.addEventListener('click', () => {
+    console.log('click');
+    
+    content3.style.display = 'block'
+
+})
+item4.addEventListener('click', () => {
+    console.log('click');
+    
+    content4.style.display = 'block'
+    
+})
+item5.addEventListener('click', () => {
+    console.log('click');
+    
+    content5.style.display = 'block'
+
+})
+item6.addEventListener('click', () => {
+    console.log('click');
+    
+    content6.style.display = 'block'
+})
+item7.addEventListener('click', () => {
+    console.log('click');
+    
+    content7.style.display = 'block'
+})
+
+item9.addEventListener('click', () => {
+    console.log('click');
+    
+    content8.style.display = 'block'
+})
+item10.addEventListener('click', () => {
+    console.log('click');
+    
+    content9.style.display = 'block'
+})
+item11.addEventListener('click', () => {
+    console.log('click');
+    
+    content10.style.display = 'block'
+})
+item12.addEventListener('click', () => {
+    console.log('click');
+    
+    content11.style.display = 'block'
+})
+item13.addEventListener('click', () => {
+    console.log('click');
+    
+    content12.style.display = 'block'
+})
+item14.addEventListener('click', () => {
+    console.log('click');
+    
+    content13.style.display = 'block'
+})
+item15.addEventListener('click', () => {
+    console.log('click');
+    
+    content14.style.display = 'block'
+})
+item16.addEventListener('click', () => {
+    console.log('click');
+    
+    content15.style.display = 'block'
+})
+item17.addEventListener('click', () => {
+    console.log('click');
+    
+    content16.style.display = 'block'
+})
+item18.addEventListener('click', () => {
+    console.log('click');
+    
+    content17.style.display = 'block'
+})
+item19.addEventListener('click', () => {
+    console.log('click');
+    
+    content18.style.display = 'block'
+})
+item20.addEventListener('click', () => {
+    console.log('click');
+    
+    content19.style.display = 'block'
+})
+item21.addEventListener('click', () => {
+    console.log('click');
+    
+    content20.style.display = 'block'
+})
+item22.addEventListener('click', () => {
+    console.log('click');
+    
+    content21.style.display = 'block'
+})
+item23.addEventListener('click', () => {
+    console.log('click');
+    
+    content22.style.display = 'block'
+})
+item24.addEventListener('click', () => {
+    console.log('click');
+    
+    content23.style.display = 'block'
+})
+item25.addEventListener('click', () => {
+    console.log('click');
+    content24.style.display = 'block'
+})
+item26.addEventListener('click', () => {
+    console.log('click');
+    
+    content25.style.display = 'block'
+})
+item27.addEventListener('click', () => {
+    console.log('click');
+    
+    content26.style.display = 'block'
+})
+item28.addEventListener('click', () => {
+    console.log('click');
+    
+    content27.style.display = 'block'
+})
+item29.addEventListener('click', () => {
+    console.log('click');
+    
+    content28.style.display = 'block'
+})
+item30.addEventListener('click', () => {
+    console.log('click');
+
+    content29.style.display = 'block'
+})
+item31.addEventListener('click', () => {
+    console.log('click');
+    
+    content30.style.display = 'block'
+})
+
+let bx = document.querySelector('.bx')
+let x = document.querySelector('#x')
+
+let x2 = document.querySelector('#x2')
+let x3 = document.querySelector('#x3')
+let x4 = document.querySelector('#x4')
+let x5 = document.querySelector('#x5')
+let x6 = document.querySelector('#x6')
+let x7 = document.querySelector('#x7')
+let x8 = document.querySelector('#x8')
+let x9 = document.querySelector('#x9')
+let x10 = document.querySelector('#x10')
+let x11 = document.querySelector('#x11')
+let x12 = document.querySelector('#x12')
+let x13 = document.querySelector('#x13')
+let x14 = document.querySelector('#x14')
+let x15 = document.querySelector('#x15')
+let x16 = document.querySelector('#x16')
+let x17 = document.querySelector('#x17')
+let x18 = document.querySelector('#x18')
+let x19 = document.querySelector('#x19')
+let x20 = document.querySelector('#x20')
+let x21 = document.querySelector('#x21')
+let x22 = document.querySelector('#x22')
+let x23 = document.querySelector('#x23')
+let x24 = document.querySelector('#x24')
+let x25 = document.querySelector('#x25')
+let x26 = document.querySelector('#x26')
+let x27 = document.querySelector('#x27')
+let x28 = document.querySelector('#x28')
+let x29 = document.querySelector('#x29')
+
+
+
+bx.addEventListener('click', () => {
+    content.style.display = 'none'
+})
+x.addEventListener('click', () => {
+    content2.style.display = 'none'
+})
+x2.addEventListener('click', () => {
+    content3.style.display = 'none'
+})
+x3.addEventListener('click', () => {
+    content4.style.display = 'none'
+})
+x4.addEventListener('click', () => {
+    content5.style.display = 'none'
+})
+x5.addEventListener('click', () => {
+    content6.style.display = 'none'
+})
+x6.addEventListener('click', () => {
+    content7.style.display = 'none'
+})
+x7.addEventListener('click', () => {
+    content8.style.display = 'none'
+})
+x8.addEventListener('click', () => {
+    content9.style.display = 'none'
+})
+x9.addEventListener('click', () => {
+    content10.style.display = 'none'
+})
+x10.addEventListener('click', () => {
+    content11.style.display = 'none'
+})
+x11.addEventListener('click', () => {
+    content12.style.display = 'none'
+})
+x12.addEventListener('click', () => {
+    content13.style.display = 'none'
+})
+x13.addEventListener('click', () => {
+    content14.style.display = 'none'
+})
+x14.addEventListener('click', () => {
+    content15.style.display = 'none'
+})
+x15.addEventListener('click', () => {
+    content16.style.display = 'none'
+})
+x16.addEventListener('click', () => {
+    content17.style.display = 'none'
+})
+x17.addEventListener('click', () => {
+    content18.style.display = 'none'
+})
+x18.addEventListener('click', () => {
+    content19.style.display = 'none'
+})
+x19.addEventListener('click', () => {
+    content20.style.display = 'none'
+})
+x20.addEventListener('click', () => {
+    content21.style.display = 'none'
+})
+x21.addEventListener('click', () => {
+    content22.style.display = 'none'
+})
+x22.addEventListener('click', () => {
+    content23.style.display = 'none'
+})
+x23.addEventListener('click', () => {
+    content24.style.display = 'none'
+})
+x24.addEventListener('click', () => {
+    content25.style.display = 'none'
+})
+x25.addEventListener('click', () => {
+    content26.style.display = 'none'
+})
+x26.addEventListener('click', () => {
+    content27.style.display = 'none'
+})
+x27.addEventListener('click', () => {
+    content28.style.display = 'none'
+})
+x28.addEventListener('click', () => {
+    content29.style.display = 'none'
+})
+x29.addEventListener('click', () => {
+    content30.style.display = 'none'
+})
